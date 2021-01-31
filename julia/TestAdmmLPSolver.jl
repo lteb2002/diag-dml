79eb5c54f0090a65a096187be3b64e63f98d5719
lteb2002 <lteb2002@163.com>
2021-01-31 16:41:04 +0800

    update

--- julia/TestAdmmLPSolver.jl
+++ julia/TestAdmmLPSolver.jl
@@ -9,13 +9,13 @@ using RereDmlLpSolverPf
 The variables in the example consist of three types:
 the first four are the main variables corresponding to the features of the dataset;
 the second four are the slack variables ;
-the third four aare the surplus variables;
+the third four are the surplus variables;
 For the details of the model, please refer to the paper 'A Fast Diagonal Distance Metric Learning Approach for Large-Scale Datasets', section 4.3
 """
 
 c=Vector([-2, -4, -1,-1,   5,5,5,5,   0,0, 0,0])
 b= Vector([8, 6, 6,9])
-A= Matrix([1.0  3.0  0.0  4.0      1  0  0  0     -1  0  0  0;
+A= Matrix([ 1.0  3.0  0.0  4.0     1  0  0  0     -1  0  0  0;
             2.0  1.0  0.0  0.0     0  1  0  0      0 -1  0  0;
             0.0  1.0  1.0  1.0     0  0  1  0      0  0 -1  0;
             1.0  1.0  1.0  0.0     0  0  0  1      0  0  0 -1])
